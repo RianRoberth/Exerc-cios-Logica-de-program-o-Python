9ddd20c8354c4585d282f2f6f5815d24776b3422
Rian <104385115+RianRoberth@users.noreply.github.com>
2026-08-26 08:49:07 -0300

    Add files via upload

---
+++ Exercicio_7.py
@@ -0,0 +1,12 @@
+def main():
+    
+    horas = float (input("Quantas horas voce trabalha? "))
+    valor_por_hora = float(input("Quanto voce recebe por hora? "))
+    horas_semanais = horas * 5 * 4
+    salario = horas_semanais * valor_por_hora
+    print(f"Seu salario é de R${salario}")
+    
+    # print(f"Seu salario é: {salario} reais")
+    
+if __name__ == "__main__":
+    main()

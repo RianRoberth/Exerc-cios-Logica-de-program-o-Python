31dc2f042b488fdc6774fd82690ff8f5944c6cc8
Rian <104385115+RianRoberth@users.noreply.github.com>
2026-08-26 08:50:53 -0300

    Calculate dollar purchase from reais input

--- exercicios_8.py
+++ exercicios_8.py
@@ -1,7 +1,11 @@
 def main():
+    
     valor_real = float(input("Valor em real R$:"))
+    
     valor_dolar = 5
+    
     valor_de_compra= valor_real // valor_dolar
+    
     print(f"Com {valor_real} reais você consegue comprar {valor_de_compra} dolares")
     
 if __name__ == "__main__":
